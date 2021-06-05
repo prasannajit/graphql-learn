3b270fbe64e6a8d412f4e193e740f9d957b6cefe
Prasannajit Tripathy <prasannajit.tripathy@tesco.com>
2021-06-05 20:54:26 +0530

    updates

--- server/schema/index.js
+++ server/schema/index.js
@@ -1,20 +1,37 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const { GraphQLNonNull, GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLList } = graphql;
 const _ = require('lodash');
 const fetch = require('cross-fetch');
+const { json } = require('express');
 
 const CompanyType = new GraphQLObjectType({
     name: 'Company',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString },
         name: { type: GraphQLString },
         description: { type: GraphQLString },
-    }
+        users: {
+            type: new GraphQLList(UserType),
+            resolve(parentValue, args) {
+                return fetch(`http://localhost:3000/companies/${parentValue.id}/users`)
+                    .then((resp) => {
+                        if (resp.status >= 400) {
+                            throw new Error(`API failed with error code ${resp.status}`)
+                        } else {
+                            return resp.json()
+                        }
+                    })
+                    .then((data) => {
+                        return data;
+                    });
+            }
+        }
+    })
 });
 
 const UserType = new GraphQLObjectType({
     name: 'User',
-    fields: {
+    fields: () => ({
         id: { type: GraphQLString },
         firstName: { type: GraphQLString },
         age: { type: GraphQLInt },
@@ -34,7 +51,7 @@ const UserType = new GraphQLObjectType({
                     });
             }
         }
-    }
+    })
 });
 
 const RootQuery = new GraphQLObjectType({
@@ -79,4 +96,82 @@ const RootQuery = new GraphQLObjectType({
     }
 })
 
-module.exports = new GraphQLSchema({ query: RootQuery });
+const mutation = new GraphQLObjectType({
+    name: 'Mutation',
+    fields: {
+        addUser: {
+            type: UserType,
+            args: {
+                firstName: { type: new GraphQLNonNull(GraphQLString) },
+                age: { type: new GraphQLNonNull(GraphQLInt) },
+                companyId: { type: GraphQLString },
+            },
+            resolve(parentValue, { firstName, age }) {
+                console.log(firstName, age);
+                return fetch(`http://localhost:3000/users`, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        firstName,
+                        age,
+                    }),
+                    headers: {
+                        'content-type': 'application/json;charset=UTF-8'
+                    }
+                }).then(resp => {
+                    if (resp.status >= 400) {
+                        throw new Error(`API failed with error code ${resp.status}`)
+                    } else {
+                        return resp.json()
+                    }
+                }).then(data => data);
+            }
+        },
+        deleteUser: {
+            type: UserType,
+            args: {
+                id: { type: GraphQLString }
+            },
+            resolve(parentValue, args) {
+                fetch(`http://localhost:3000/users/${args.id}`, {
+                    method: 'DELETE'
+                }).then((resp) => {
+                    if (resp.status >= 400) {
+                        throw new Error(`API failed with error code ${resp.status}`)
+                    } else {
+                        return resp.json()
+                    }
+                }).then(data => data);
+            }
+        },
+        editUser: {
+            type: UserType,
+            args: {
+                id: { type: new GraphQLNonNull(GraphQLString) },
+                firstName: { type: GraphQLString },
+                age: { type: GraphQLInt },
+                companyId: { type: GraphQLString }
+            },
+            resolve(parentValue, { id, firstName, age, companyId }) {
+                fetch(`http://localhost:3000/users/${id}`, {
+                    method: 'PATCH',
+                    body: JSON.stringify({
+                        firstName,
+                        age,
+                        companyId
+                    }),
+                    headers: {
+                        'content-type': 'application/json;charset=UTF-8'
+                    }
+                }).then((resp) => {
+                    if (resp.status >= 400) {
+                        throw new Error(`API failed with error code ${resp.status}`)
+                    } else {
+                        return resp.json()
+                    }
+                }).then(data => { console.log(data); return data; });
+            }
+        }
+
+    }
+})
+module.exports = new GraphQLSchema({ query: RootQuery, mutation });
